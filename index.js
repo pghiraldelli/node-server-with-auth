858686951a9eebc694cadd3081c03cd68f69711c
Patricia <psantosg@thoughtworks.com>
2019-10-05 15:06:12 -0300

    Adds dotenv package to read env vars

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 var app = require('./config/app');
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.listen(8000, () => console.log('Server running on http://localhost:8000/'));
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
